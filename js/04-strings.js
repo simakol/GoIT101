ceea83b1c02ef9a5bde50539fb44df90e5f65869
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-01-09 19:22:20 +0200

    lesson-01

---
+++ js/04-strings.js
@@ -0,0 +1,30 @@
+/**
+ * Рядки
+ *
+ * - Конкатенація рядків
+ * - Шаблонні рядки
+ * - Довжина рядка
+ * - Індексація елементів
+ * - Незмінність рядків
+ */
+
+const firstName = "Chelsy";
+const lastName = "Emerald";
+// console.log(fullName);
+
+const quantity = 15;
+const orderMsg = "You ordered ??? bots";
+// console.log(orderMsg);
+
+const message = "This string is 28 chars long";
+// console.log(message);
+
+/**
+ * Склади фразу за допомогою шаблонних рядків
+ * A has B bots in stock, де A, B - змінні вставлені в рядок.
+ */
+const companyName = "Cyberdyne Systems";
+const repairBots = 150;
+const defenceBots = 50;
+// const msg = ;
+// console.log(msg); // "Cyberdyne Systems has 200 bots in stock"
---
+++ js/05-coercion.js
@@ -0,0 +1,18 @@
+/**
+ * Перетворення типів: рядки
+ *
+ * - Явне перетворення типів
+ * - Неявне перетворення типів
+ */
+
+console.log(String(5)); // "5"
+console.log(String(true)); // "true"
+console.log(String(false)); // "false"
+console.log(String(null)); // "null"
+console.log(String(undefined)); // "undefined"
+
+console.log("5" + 3); // "53"
+console.log("5" + true); // "5true"
+console.log("5" + false); // "5false"
+console.log("5" + null); // "5null"
+console.log("5" + undefined); // "5undefined"
