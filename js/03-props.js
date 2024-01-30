a0acedeb0c3add659719eb69095a6cbb568da694
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-01-30 18:42:55 +0200

    lesson-07

---
+++ js/03-props.js
@@ -0,0 +1,25 @@
+/**
+ * Об'єкти
+ *
+ * - Короткі властивості
+ * - Обчислювальні властивості
+ */
+
+const username = "Mango";
+const email = "mango@mail.com";
+
+const credentials = {};
+
+console.log(credentials);
+console.log(credentials.username);
+console.log(credentials.email);
+
+/**
+ * -------------------------
+ */
+
+const inputName = "color";
+
+const colorPickerData = {};
+
+console.log(colorPickerData);
---
+++ js/04-iteration.js
@@ -0,0 +1,30 @@
+/**
+ * Перебір об'єкта
+ *
+ * - Цикл for...in
+ * - Метод Object.keys
+ * - Метод Object.values
+ */
+
+const feedback = {
+  good: 5,
+  neutral: 10,
+  bad: 3,
+};
+
+let totalFeedback = 0;
+
+// console.log("totalFeedback: ", totalFeedback);
+
+/**
+ * ---------------------------------
+ */
+const keys = feedback;
+console.log(keys);
+
+// console.log("totalFeedback: ", totalFeedback);
+
+const values = feedback;
+console.log(values);
+
+// console.log('totalFeedback: ', totalFeedback);
